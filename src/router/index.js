38c18ccbc1e97486b1098daea2f1766023adba62
Ponkin Dmitrij <ponkin.dmitrij@gmail.com>
2019-04-02 12:05:47 +0300

    Adds JSDoc comments

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,17 @@ const PAGE_URL = {
   login: '/page/login/',
 };
 
+/**
+ * Goes by url.
+ * @param {String} url - The path to the page.
+ */
 const goTo = url => {
   location = `${location.origin}/src${url}`;
 };
 
+/**
+ * Returns to the previous page.
+ */
 const goBack = () => {
   window.history.back();
 };
